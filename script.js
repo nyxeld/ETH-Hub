84f761ea75932847904d1bde8f07b862d7aed312
nyxeld <119258995+nyxeld@users.noreply.github.com>
2025-08-30 01:18:13 +0200

    burger menu redesign
    finally reworked and fixed the burger menu on hopefully any screen

--- script.js
+++ script.js
@@ -181,6 +181,8 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', (event) => {
             event.preventDefault();
             window.location.hash = 'home';
+            navMenu.classList.toggle('active');
+            burgerMenu.classList.toggle('active');
         });
     });
 
